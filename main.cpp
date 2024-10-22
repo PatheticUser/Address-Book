d46b56d224156e560aad3d2e58786d870bda5781
Rameez <148254423+PatheticUser@users.noreply.github.com>
2024-10-22 18:38:50 +0500

    Added contact details functionality and menu options
    - Extended `AddContacts` function to include contact name, gender, age, phone number, and address.
    - Implemented `ShowContacts` function to display all added contacts with their details.
    - Updated menu to reflect the new "Add a new buddy" and "Show my buddies" options.
    - Improved user interaction with input validation for gender.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@ using namespace std;
 struct ContactInfo {
     string cName;
     int cGender = 1;
+    int cAge = 0;     // Added cAge
+    string cPhone;    // Added cPhone
+    string cAdd;      // Added cAdd
 };
 
 struct ContactBook {
@@ -18,12 +21,63 @@ void showMenu() {
     cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
     cout << "~~~  Welcome to Your Book  ~~~" << endl;
     cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+    cout << "~~~ 1. Add a new buddy      ~~~" << endl;  // Added menu option
+    cout << "~~~ 2. Show my buddies      ~~~" << endl;  // Added another option
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
 }
 
 void AddContacts(ContactBook* myBook) {
     if (myBook->count == Max) {
         cout << "Yo! Book's full, no more space! :(" << endl;
         return;
+    } else {
+        string name;
+        cout << "What's the name? " << endl;
+        cin >> name;
+        myBook->contactList[myBook->count].cName = name;
+
+        int gender;
+        cout << "Gender? (1 = Male, 2 = Female)" << endl;
+        while (true) {
+            cin >> gender;
+            if (gender == 1 || gender == 2) {
+                myBook->contactList[myBook->count].cGender = gender;
+                break;
+            }
+            cout << "C'mon, enter a valid gender! :P" << endl;
+        }
+
+        int age;
+        cout << "Age? " << endl;
+        cin >> age;
+        myBook->contactList[myBook->count].cAge = age;
+
+        string phone;
+        cout << "Phone number? " << endl;
+        cin >> phone;
+        myBook->contactList[myBook->count].cPhone = phone;
+
+        string add;
+        cout << "Address? " << endl;
+        cin >> add;
+        myBook->contactList[myBook->count].cAdd = add;
+
+        myBook->count++;
+        cout << "Sweet! Added your buddy! :)" << endl;
+    }
+}
+
+void ShowContacts(ContactBook* myBook) {
+    if (myBook->count == 0) {
+        cout << "Oops, you got no friends here... Add some first! :D" << endl;
+    } else {
+        for (int i = 0; i < myBook->count; i++) {
+            cout << "Name: " << myBook->contactList[i].cName
+                 << " Gender: " << (myBook->contactList[i].cGender != 2 ? "Male" : "Female")
+                 << " Age: " << myBook->contactList[i].cAge
+                 << " Phone: " << myBook->contactList[i].cPhone
+                 << " Address: " << myBook->contactList[i].cAdd << endl;
+        }
     }
 }
 
@@ -39,6 +93,9 @@ int main() {
         switch (option) {
         case 1:
             AddContacts(&myBook);
+            break;
+        case 2:
+            ShowContacts(&myBook);
             break;
         }
     }
